9dbcd498dbb3ec420d51b8018e36e9e8a3a93fe9
Sarah Mirsaidi Madjdabadi <smirsaidimadjdabadi@ucsd.edu>
2025-05-20 11:55:36 -0700

    add class4

---
+++ class04/class04.R
@@ -0,0 +1,11 @@
+#' ---
+#' title: "Intro to R"
+#' author: "Sarah Mirsaidi Madjdabadi"
+#' date: "April 10, 2025"
+#' ---
+
+# My first R script
+x <- 1:50
+plot(x, sin(x))
+plot(x, sin(x), typ="l", col="pink", lwd=3, xlab="Silly x axis", 
+     ylab="Sensible y axis")
